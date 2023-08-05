c38d84a9d69a1382c9ba4169bc0490bb6aef5a6f
TrishalaGhetiya <115083359+TrishalaGhetiya@users.noreply.github.com>
2023-08-05 16:39:08 +0530

    created new routes, page not found, filtering

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
-    next();
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page not Found</h1>");
 })
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><br><input type="number" name"quantity"><br><button type="submit">Add Product</button></form>')
-})
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-    res.send('Hello');
-})
 app.listen(4000);
 
