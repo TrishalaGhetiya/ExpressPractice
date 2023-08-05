b2f2e4d1c4cede59a014f062f66b147dbbc58a46
TrishalaGhetiya <115083359+TrishalaGhetiya@users.noreply.github.com>
2023-08-05 13:05:31 +0530

    Created basic express code

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    next();
+})
+
+
+app.use((req, res, next) => {
+    res.send('Hello!');
+})
+
+app.listen(4000);
+
